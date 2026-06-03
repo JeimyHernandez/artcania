1a31b1c2a6ae2842056e95f744967d5d92bdd639
RichardR99 <richardgame1999@gmail.com>
2026-06-02 20:00:15 -0600

    Update chat_artista.php
    modificacion en codigo

--- views/usuario/chat_artista.php
+++ views/usuario/chat_artista.php
@@ -1,6 +1,3 @@
-<?php
-// Redirect to unified chat view
-$cfg = artcania_config();
-$url = rtrim($cfg['url'], '/') . '/chat';
-header('Location: ' . $url, true, 302);
-exit;
+
+<?php $pageTitle = ''; ?>
+<?php header('Location: '.url('perfil')); exit; ?>
